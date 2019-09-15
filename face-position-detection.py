52ad8fc65938ceb8ff70a760c59262c877c2a9c9
djonnyx <djonnyx@gmail.com>
2019-09-15 20:05:29 +0300

    feat: implemented sending coordinates by usb

--- face-position-detection.py
+++ face-position-detection.py
@@ -8,41 +8,51 @@ import sensor, time, image, pyb, math
 sensor.reset()
 
 # setup sensor's
+sensor.reset()
 sensor.set_framesize(sensor.HQQVGA)
 sensor.set_pixformat(sensor.GRAYSCALE)
 sensor.set_gainceiling(2)
 sensor.set_contrast(3)
 sensor.skip_frames()
 
+# initialization usb/uart
+usb = pyb.USB_VCP()
+
+MAX_OFFSET = 30
+RESET_TRACKING_TIMEOUT = 60
+SKIP_FRAMES = 2
+
+elapsed_to_reset = RESET_TRACKING_TIMEOUT
+s_frames = SKIP_FRAMES
+
 # HQQVGA half size
-half_width = 120 * 0.5
-half_height = 80 * 0.5
+hw = 120 * 0.5
+hh = 80 * 0.5
 
 isReseted = True
-positions = [[0.0, 0.0]]
+epos = [0.0, 0.0]
+lpos = [0.0, 0.0]
+poss = [[0.0, 0.0]]
 
 # send coordinates
 def send(pos):
     f = b"%f:%f\n" % (pos[0], pos[1])
-    print(f)
-
-def getLen(x, y):
-  return math.fabs(x + y) * 0.5
+    usb.send(f)
 
 def addPosition(x, y):
     sx = 0
     sy = 0
-    length = len(positions)
-    for p in positions:
+    l = len(poss)
+    for p in poss:
       sx += p[0]
       sy += p[1]
 
-    img.draw_cross(int(sx), int(sy), 0xffffff, 4)
+    # img.draw_cross(int(sx), int(sy), 0xffffff, 4)
 
-    norm_x = (sx / length - half_width) / half_width
-    norm_y = (sy / length - half_height) / half_height
+    nx = (sx/l-hw)/hw
+    ny = (sy/l-hh)/hh
 
-    send([norm_x, norm_y])
+    send([nx, ny])
 
 # initialize haar
 face_cascade = image.HaarCascade("frontalface", stages=20)
@@ -50,6 +60,10 @@ face_cascade = image.HaarCascade("frontalface", stages=20)
 while (True):
     elapsed_to_reset -= 1
     s_frames -= 1
+
+    if elapsed_to_reset < 0:
+        isReseted = True
+        elapsed_to_reset = RESET_TRACKING_TIMEOUT
 
     # capture image
     img = sensor.snapshot()
@@ -62,8 +76,20 @@ while (True):
         x = r[0] + r[2] * 0.5
         y = r[1] + r[3] * 0.5
 
-        positions.pop(0)
-        positions.append([x, y])
+        dx = math.fabs(lpos[0] - x)
+        dy = math.fabs(lpos[1] - y)
 
+        if isReseted == False and (dx > MAX_OFFSET or dy > MAX_OFFSET):
+            continue
+
+        poss.pop(0)
+        poss.append([x, y])
+
+        if s_frames > 0:
+            s_frames = SKIP_FRAMES
         addPosition(x, y)
+
+        lpos[0] = x
+        lpos[1] = y
+        isReseted = False
         break
